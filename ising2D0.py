a618afab6a4a7b6e97d2e5f3cb867db62a80afb9
Ignacio Poggi <ignaciop.3@gmail.com>
2018-06-13 23:09:25 -0300

    0

--- ising2D0.py
+++ ising2D0.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 from matplotlib import pyplot
-from random import randint
 
 #############################################################
 # estas son las funciones que tienen que escribir ustedes
@@ -17,44 +16,36 @@ def calcMagnet(S):
 
 def calcEnergia(S):
 	e = 0
-	for i in range(L-1):
-		for j in range(L-1):
-			if (i == L-1):
+	for i in range(len(S)):
+		for j in range(len(S)):
+			if (i == len(S)-1):
 				i = -1
 	
-			if (j == L-1):
+			if (j == len(S)-1):
 				j = -1
-		
-			prox_vecinos = np.zeros(2)
-			prox_vecinos[0] = S[i+1][j]
-			prox_vecinos[1] = S[i][j+1]
 	
-			e = e + S[i][j]*sum(prox_vecinos)
+			e += -S[i][j]*(S[i+1][j]+S[i][j+1])
 			
-	e = -0.5*e
+	#e = e/2
 	return e
 
 def ising2Dpaso(S, beta):
-	i = randint(0,L-1)
-	j = randint(0,L-1)
+	i = np.random.randint(0,len(S))
+	j = np.random.randint(0,len(S))
 	
-	if (i == L-1):
+	if (i == len(S)-1):
 		i = -1
 	
-	if (j == L-1):
+	if (j == len(S)-1):
 		j = -1
-		
-	prox_vecinos = np.zeros(2)
-	prox_vecinos[0] = S[i+1][j]
-	prox_vecinos[1] = S[i][j+1]
 	
-	dE = 2*S[i][j]*sum(prox_vecinos)
+	dE = 2*S[i][j]*(S[i+1][j]+S[i][j+1])
 	
-	if ((dE <= 0) or (np.random.rand() <= np.exp(-beta*dE))):
-		S[i][j] = -S[i][j]
+	if ((dE <= 0) or (np.random.rand() < np.exp(-beta*dE))):
+		S[i][j] *= -1
     #    return nuevo_S, dE, dM
 
-	dM = np.sum(S)
+	dM = S[i][j]
 	return S, dE, dM
 
 #############################################################
@@ -71,7 +62,7 @@ beta = 0.1
 S = 2*(np.random.rand(L,L)>0.5) -1;
 
 npre = 100
-npasos = 1000
+npasos = 200000
 energia= np.zeros(npasos)
 magnet = np.zeros(npasos)
 
@@ -82,8 +73,8 @@ for n in range(npre):
     S, dE, dM = ising2Dpaso(S,beta)
 
 # muestro el estado inicial
-pyplot.imshow(S,interpolation='none')
-pyplot.show(block=False)
+#pyplot.imshow(S,interpolation='none')
+#pyplot.show(block=False)
 
 energia[0] = calcEnergia(S)
 magnet[0] = calcMagnet(S)
@@ -94,7 +85,10 @@ for n in range(npasos-1):
 	magnet[n+1] = magnet[n] + dM;
 	
     #cada 10 pasos muestro el nuevo estado
-	if n%10 == 0:
-		pyplot.imshow(S,interpolation='none')
-		pyplot.title("n=%i beta=%.2f mag=%.2f energia=%.2f"%(n,beta,magnet[n],energia[n]))
-		pyplot.show()
+	#if n%10 == 0:
+	#	pyplot.imshow(S,interpolation='none')
+	#	pyplot.title("n=%i beta=%.2f mag=%.2f energia=%.2f"%(n,beta,magnet[n],energia[n]))
+	#	pyplot.show()
+
+pyplot.plot(magnet/(L*L))
+pyplot.show()
